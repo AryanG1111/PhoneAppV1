2326a2428e22262257fa27d24b6a94f5f07cb24a
AryanG1111 <arygad11@gmail.com>
2023-01-11 19:29:44 -0500

    Added new screens and adding new java classes

---
+++ app/src/main/java/com/example/phoneappv1/Riddle.java
@@ -0,0 +1,13 @@
+package com.example.phoneappv1;
+
+public class Riddle {
+    private String question;
+    private String answer;
+
+    public Riddle(String q, String a) {
+        question = q;
+        answer = a;
+    }
+
+
+}
